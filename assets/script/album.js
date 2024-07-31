4c29cb4b03a201763e27ee1e88a612ea2ac441f4
d-mala <davide.malavenda26@gmail.com>
2024-07-31 10:41:38 +0200

    added genDynamicBg function to album.js

--- assets/script/album.js
+++ assets/script/album.js
@@ -1,57 +1,55 @@
-const addressBarParameters = new URLSearchParams(location.search);
-const albumID = addressBarParameters.get("albumId");
+const addressBarParameters = new URLSearchParams(location.search)
+const albumID = addressBarParameters.get('albumId')
 
-
-const playsong = function (mp3){
+const playsong = function (mp3) {
   console.log(mp3)
-  const song = new Audio (`${mp3}`)
+  const song = new Audio(`${mp3}`)
   song.play()
 }
 
 const getAlbum = function () {
   fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${albumID}`)
     .then((resp) => {
-      console.log(resp);
-      if (resp.ok) return resp.json();
-      else throw new Error("errore nella chiamata della api");
+      console.log(resp)
+      if (resp.ok) return resp.json()
+      else throw new Error('errore nella chiamata della api')
     })
     .then((data) => {
-      console.log(data);
-      getartistinfo(data);
-      getalbuminfo(data);
-      getsong(data);
+      console.log(data)
+      getartistinfo(data)
+      getalbuminfo(data)
+      getsong(data)
+      genDynamicBg()
     })
     .catch((err) => {
-      console.log("Error", err);
-    });
-};
+      console.log('Error', err)
+    })
+}
 
 const getartistinfo = function (album) {
-  const artistImg = document.getElementById("artist-photo");
-  const artistname = document.getElementById("artist-name");
-  artistImg.setAttribute("src", `${album.contributors[0].picture}`);
-  artistname.innerText = album.contributors[0].name;
-};
+  const artistImg = document.getElementById('artist-photo')
+  const artistname = document.getElementById('artist-name')
+  artistImg.setAttribute('src', `${album.contributors[0].picture}`)
+  artistname.innerText = album.contributors[0].name
+}
 
 const getalbuminfo = function (album) {
-  const albumimg = document.getElementById("album-cover");
-  const albumtitle = document.getElementById("album-title");
-  const albumyear = document.getElementById("album-year");
-  const albumtracks = document.getElementById("n-of-songs");
-  const albumduration = document.getElementById("album-duration");
-  albumimg.setAttribute("src", `${album.cover_medium}`);
-  albumtitle.innerText = album.title;
-  albumyear.innerText = album.release_date.slice(0, 4);
+  const albumimg = document.getElementById('album-cover')
+  const albumtitle = document.getElementById('album-title')
+  const albumyear = document.getElementById('album-year')
+  const albumtracks = document.getElementById('n-of-songs')
+  const albumduration = document.getElementById('album-duration')
+  albumimg.setAttribute('src', `${album.cover_medium}`)
+  albumtitle.innerText = album.title
+  albumyear.innerText = album.release_date.slice(0, 4)
   albumtracks.innerText = album.nb_tracks
   const hourdur = Math.floor(album.duration / 60)
-  const mindur = album.duration - (hourdur * 60)
+  const mindur = album.duration - hourdur * 60
   albumduration.innerText = `${hourdur} ore e ${mindur} minutes`
-
-};
-
+}
 
 const getsong = function (album) {
-  const artist = album.contributors[0].name;
+  const artist = album.contributors[0].name
   album.tracks.data.forEach((track) => {
     console.log(track)
     const createdli = `        
@@ -62,16 +60,30 @@ const getsong = function (album) {
           </div> 
         </li>`
 
-    const songlist = document.getElementById("song-list");
+    const songlist = document.getElementById('song-list')
     songlist.innerHTML = songlist.innerHTML + createdli
-  });
-};
+  })
+}
 
+// rendo dinamico il bg di album con la libreria color thief
+
+const genDynamicBg = function () {
+  const albumimg = document.getElementById('album-cover')
+
+  const colorThief = new ColorThief()
+
+  const dominantColor = colorThief.getColor(albumimg)
+
+  document.documentElement.style.setProperty(
+    '--primary-color',
+    `rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`)
+}
 
 const init = function () {
-  getAlbum();
-};
+  getAlbum()
+  genDynamicBg()
+}
 
-window.addEventListener("load", function () {
-  init();
-});
+window.addEventListener('load', function () {
+  init()
+})
