1c5bfd555a0e02450d98379d0ec39bd5720e113c
antoniocostantin <costantini062@gmail.com>
2024-08-01 15:09:39 +0200

    devo mergiare

---
+++ assets/script/search.js
@@ -0,0 +1,21 @@
+const getSearch = function (search) {
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${search}}`)
+      .then((resp) => {
+        if (resp.ok) return resp.json();
+        else {
+          throw new Error("errore nella chiamata della api");
+        }
+      })
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {});
+  };
+
+// const formHTML = document.getElementById("")
+// const oursearch = formHTML.value
+ const mainHTML = document.getElementsByTagName("main")
+// formHTML.addEventListener("submit", function(){
+//     getSearch(oursearch)
+// })
+getSearch("caparezza")
