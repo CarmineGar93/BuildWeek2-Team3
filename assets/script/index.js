2d2493a3251ce887ae1cd9d1b20636e815b4ef81
antoniocostantin <costantini062@gmail.com>
2024-08-02 00:52:01 +0200

    ultimo commit sul main sorry ma sono aggiustamenti e domani nn avrò il pc

--- assets/script/index.js
+++ assets/script/index.js
@@ -143,6 +143,61 @@ const getRandomSong = function (a_b, list) {
     .catch((err) => {});
 };
 
+const writebanner = function(album){
+  const bannerhtml = document.getElementById("banner")
+
+  banner = `
+  <div class="banner-img">
+    <img src="${album.cover_xl}" alt="album" class="img-fluid" />
+  </div>
+  <div class="d-flex flex-column justify-content-center gap-2">
+    <a class="h4 text-decoration-none text-white mb-1" href="album.html?albumId=${album.id}"><h6>${album.title}</h6></a>
+    <a id="banner-artist" type="button" class="mb-3 text-decoration-none text-white" href="artisti.html?artistiId=${album.artist.id}">${album.artist.name}</a>
+    <div class="d-flex gap-4 align-items-center mt-3">
+      <a class="text-decoration-none text-white mb-1" href="album.html?albumId=${album.id}">
+      <button id="banner-play" class="btn btn-success rounded-pill text-dark">
+      Play
+      </button></a>
+      <button class="btn btn-outline-light rounded-pill">
+        Save
+      </button>
+      <div>
+        <svg data-encore-id="icon" role="img" aria-hidden="true" viewBox="0 0 16 16"
+          class="Svg-sc-ytk21e-0 dYnaPI" width="17" height="17" fill="#A7A7A7">
+        <path
+          d="M3 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm6.5 0a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zM16 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z">
+        </path>
+        </svg>
+      </div>
+    </div>
+    </div>`;
+
+    bannerhtml.innerHTML = banner
+
+}
+
+const getBanner = function (a_b, list) {
+  const id = randomInt(0, 999999);
+  fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${id}`)
+    .then((resp) => {
+      if (resp.ok) return resp.json();
+      else {
+        getBanner(); // se la risposta non è ok allora richiama la funzione
+        throw new Error("errore nella chiamata della api");
+      }
+    })
+    .then((data) => {
+      console.log(data)
+      if (data.error) getBanner();
+      if (data.fans < 400) getBanner();
+      else {
+        console.log(data)
+        writebanner(data)
+      }
+    })
+    .catch((err) => {});
+};
+
 const writeAlbum = function (album, list, n) {
   const gennarolist_item = `
     <li class="my-2 fs-6 d-flex align-content-center">
@@ -179,6 +234,7 @@ const getRand = function (list, string) {
 };
 
 const init = function () {
+  getBanner()
   const gennarolist = document.querySelector("ul.list-unstyled");
   gennarolist.innerHTML = "";
   for (let loop = 0; loop < 10; loop++) {
